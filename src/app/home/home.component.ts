d3579946b42ecee78118da10ee39fe30e3b7e565
hidagara <hidagara@gmail.com>
2018-02-18 15:06:09 +0300

    {observable} create observable and handle data,

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
+import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,  OnDestroy {
 
+  numberOfSubscriptions: Subscription;
+  customSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.intervalExample();
+    this.completedOwnObservableExample();
+  }
+
+  ngOnDestroy() {
+
+  }
+
+
+
+  errorOwnObservableExample(): void {
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.error('eror while sending data');
+      }, 6000);
+      });
+    myObservable.subscribe(
+      (data: string) => {
+      console.log(data);
+    }, (error: string) => {console.log(error); },
+      () => {console.log('operation completed'); }
+    );
+  }
+
+  completedOwnObservableExample(): void {
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.complete();
+      }, 6000);
+      setTimeout(() => {
+        observer.next('package after complete, will not send');
+      }, 7000);
+    });
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      }, (error: string) => {console.log(error); },
+      () => {console.log('operation completed'); }
+    );
+  }
+
+  intervalExample(): void {
     const myNumbers = Observable.interval(1000);
     myNumbers.subscribe(
       (number: number) => {
